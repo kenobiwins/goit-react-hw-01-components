e925ab2951fbd8948621e4e0527346e6de51d9fe
Max Sidorenko <fivemoshr@gmail.com>
2022-11-26 22:52:12 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,27 @@ import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import { Box } from "./Box";
+import { ThemeProvider } from "styled-components";
 
+import {theme} from '../theme.js';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
 import transactions from '../transactions.json';
 
+
 export const App = () => {
   return (
     <>
-      <Profile user={user}/>
-      <Statistics title={"Upload stats"} stats={data}/>
-      <FriendList friends={friends} />
-      <TransactionHistory transactions={transactions} />
+      <ThemeProvider theme={theme}>
+        <Box>
+          <Profile user={user} />
+          <Statistics title={"Upload stats"} stats={data} />
+          <FriendList friends={friends} />
+          <TransactionHistory transactions={transactions} />
+        </Box>
+      </ThemeProvider>
     </>
   );
 };
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,23 @@
 import styled from 'styled-components';
 
-import { grid, color, space, display } from 'styled-system';
-
-export const Box = styled('div')({
-  display,
-
-  grid,
+import {
   color,
   space,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
+} from 'styled-system';
+
+export const Box = styled('div')({
+  color,
+  space,
+  layout,
+  flexbox,
+  grid,
+  background,
+  border,
+  position,
 });
